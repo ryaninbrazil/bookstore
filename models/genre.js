f5ff69960cf3b3c0efd1aa1f79da225d3f1b805d
Ryan <ryaninbrazil@gmail.com>
2017-11-24 20:59:39 +0000

    first commit

---
+++ models/genre.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+// Genre Schema
+
+const genreSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  create_date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const
